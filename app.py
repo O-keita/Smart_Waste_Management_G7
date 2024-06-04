00912b5bb7dade37b625b39004d4b6a3fd2c8f69
O-keita <o.keita@alustudent.com>
2024-06-04 18:54:21 +0000

    user login and registration added

--- app.py
+++ app.py
@@ -1,10 +1,29 @@
 from flask import Flask, render_template
+from forms import Registration, LoginForm
+
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'b7318d5b51927a4dc8f7fad696408fc4'
 
 @app.route('/')
 def hello():
     return render_template('home.html')
+
+@app.route('/login')
+def login():
+
+    form = LoginForm()
+
+    return render_template('login.html', form=form)
+
+
+@app.route('/register')
+def register():
+
+    form = Registration()
+
+    return render_template('register.html', form=form)
+
 
 
 if __name__ == '__main__':
