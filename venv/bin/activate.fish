f9d717f0490460986924c280304383792f7dfd90
khalid <K.abdillahi@alustudent.com>
2024-06-11 18:57:48 +0200

    khalo

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/omar/alu_projects/Smart_Waste_Management_G7/venv"
+set -gx VIRTUAL_ENV "/home/khalid/Smart_Waste_Management_G7/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
